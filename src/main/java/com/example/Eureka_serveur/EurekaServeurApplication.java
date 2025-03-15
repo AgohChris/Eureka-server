636ae004a78955c89d89c1603374e52da164287f
PyInv🐍 <pyinv@Py.local>
2025-03-15 06:52:09 +0000

    first auto commit : 15-03-2025 à 06:52:08

---
+++ src/main/java/com/example/Eureka_serveur/EurekaServeurApplication.java
@@ -0,0 +1,13 @@
+package com.example.Eureka_serveur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServeurApplication.class, args);
+	}
+
+}
